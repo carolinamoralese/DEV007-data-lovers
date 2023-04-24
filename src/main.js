77fd23c97d88769ea9c8636f8cc4ba1231accf70
carolinamoralese <caromorales1396@gmail.com>
2023-04-24 18:38:39 -0500

    agreho segundo cambio, se modifico la tarjeta de peliculas

--- src/main.js
+++ src/main.js
@@ -3,22 +3,26 @@
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 const dataStudioGhibli = data
+const homeScreen=document.getElementById("root")
 let cardContainer =document.getElementById("card-container")
 let optionFilms =
+
 //forma de mostar el nombrede un personaje, primero ingreso a peliculas, people,  nombre
-//console.log(data.films[6].people[4].name);
+console.log(data.films[6].people[4].name);
 
 
 const buttonPeliculas = document.getElementById("movies")
 buttonPeliculas.addEventListener("click",showMovies)
 
 function showMovies(){
+    homeScreen.style.display="none"
     dataStudioGhibli.films.forEach((film) => {
         optionFilms = `
-        <div name= "films" id=${film.title}>
-        <p>${film.title}</p>
-        <img src=${film.poster}>
-        </div> `
+        <div name= "films"  id=${film.title}>
+        <p class="title-card">${film.title}</p>
+        <img class="card" src =${film.poster} >
+        </div> 
+        `
        
         cardContainer.innerHTML += optionFilms
    
