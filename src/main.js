9ecab69d1c229d7681f870ae0695f2274862c003
carolinamoralese <caromorales1396@gmail.com>
2023-04-24 16:25:13 -0500

    agrego los primeros cambios, se inicio el HTML-CSS-JS

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+const dataStudioGhibli = data
+let cardContainer =document.getElementById("card-container")
+let optionFilms =
+//forma de mostar el nombrede un personaje, primero ingreso a peliculas, people,  nombre
+//console.log(data.films[6].people[4].name);
 
-console.log(example, data);
+
+const buttonPeliculas = document.getElementById("movies")
+buttonPeliculas.addEventListener("click",showMovies)
+
+function showMovies(){
+    dataStudioGhibli.films.forEach((film) => {
+        optionFilms = `
+        <div name= "films" id=${film.title}>
+        <p>${film.title}</p>
+        <img src=${film.poster}>
+        </div> `
+       
+        cardContainer.innerHTML += optionFilms
+   
+})
+}
